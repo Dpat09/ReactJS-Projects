cb978cd53abf6b5f4fce1ebfc472160245ce274c
Dpat09 <Dpat092@gmail.com>
2018-07-23 23:13:48 -0500

    Added props to handle deleting of components

--- reactCrashCourse/counter-app/src/components/counter.jsx
+++ reactCrashCourse/counter-app/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 // https://www.youtube.com/watch?v=Ke90Tje7VS0 55:02
 class Counter extends Component {
   state = {
-    count: 0
+    value: this.props.counter.value
   };
 
   // constructor() {
@@ -10,12 +10,12 @@ class Counter extends Component {
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
@@ -23,19 +23,25 @@ class Counter extends Component {
         >
           Increment
         </button>
-      </React.Fragment>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
