908565a496a74583bf58e53cc031ced3a5bb4562
Dpat09 <Dpat092@gmail.com>
2018-07-23 23:45:39 -0500

    Added navbar now we have a single source of truth (removed local states)

--- reactCrashCourse/counter-app/src/components/counter.jsx
+++ reactCrashCourse/counter-app/src/components/counter.jsx
@@ -1,24 +1,11 @@
 import React, { Component } from "react";
-// https://www.youtube.com/watch?v=Ke90Tje7VS0 55:02
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value
-  };
-
-  // constructor() {
-  //   super();
-  //   this.handleIncrement = this.handleIncrement.bind(this);
-  // }
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -35,12 +22,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
